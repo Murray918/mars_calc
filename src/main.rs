65b83418b3a25219214d2ff2c8a363348200fdb8
Andrew Murray <darbyguitars@gmail.com>
2020-08-19 12:05:36 -0500

    convert weight to grams
     Changes to be committed:
    	modified:   src/main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
 fn main() {
-    println!("weight: {}",  calculate_our_weight_on_mars(100.0));
+    let mut mars_weight = calculate_our_weight_on_mars(100.0);
+
+    mars_weight = mars_weight * 1000.0;
+
+    println!("weight: {}", mars_weight);
 }
 
 fn calculate_our_weight_on_mars(weight: f32) -> f32 {
