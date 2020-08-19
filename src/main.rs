dda7d89ac48e81f7cec14b34c59d8727d8988a6a
Andrew Murray <darbyguitars@gmail.com>
2020-08-19 12:12:11 -0500

    take user input
     Changes to be committed:
    	modified:   src/main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,11 @@
-fn main() {
+use std::io;
+
+
+fn main() { 
+    let mut input = String::new();
+
+    io::stdin().read_line(&mut input);
+
     let mut mars_weight = calculate_our_weight_on_mars(100.0);
 
     mars_weight = mars_weight * 1000.0;
